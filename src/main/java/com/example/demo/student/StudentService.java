9982ccabe5b856237cf1f2c6ed837c37c82de878
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-10-25 15:05:27 +0600

    server started with json response

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,28 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+//StudentService must be instantiated
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(
+                1L,
+                "Nafees Iqbal",
+                "Nafees@gmail.com",
+                LocalDate.of(2000, Month.JANUARY,5),
+                21));
+
+        return list;
+
+    }
+}
